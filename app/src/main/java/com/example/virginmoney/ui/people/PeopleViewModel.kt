27c0013ae2816089733d367aec7c11e0ac93137c
jnmii <mbuyambajoseph@gmail.com>
2023-07-11 08:40:35 -0400

    google sign in

--- app/src/main/java/com/example/virginmoney/ui/people/PeopleViewModel.kt
+++ app/src/main/java/com/example/virginmoney/ui/people/PeopleViewModel.kt
@@ -1,7 +1,34 @@
 package com.example.virginmoney.ui.people
 
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.virginmoney.data.model.people.PeopleModel
 import com.example.virginmoney.data.repository.Repository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 
-class PeopleViewModel(repository: Repository) {
-}
+@HiltViewModel
+class PeopleViewModel @Inject constructor(
+    private val repository: Repository
+) : ViewModel() {
+
+    val peopleLiveData : MutableLiveData<List<PeopleModel>> by lazy {
+        MutableLiveData<List<PeopleModel>>()
+    }
+
+    var isLoaded = false
+
+    fun getPeopleData() {
+
+        CoroutineScope(Dispatchers.Main).launch {
+            val result = repository.getAllPeople()
+            peopleLiveData.postValue(result)
+
+            isLoaded = true
+
+        }
+    }
